a4907924e7b84c329efef79d75d6d0f1d950d9dd
Yadunandan Batchu <nandubatchu@gmail.com>
2020-04-08 23:50:38 +0530

    remove unused code

--- src/data-connectors.ts
+++ src/data-connectors.ts
@@ -1,3 +1,2 @@
-export { InMemoryDataConnector }  from "./in-memory-data-connector";
 export { SequelizeDataConnector } from "./sequelize-data-connector";
 export type DataConnectorType = "InMemoryDataConnector" | "SequelizeDataConnector";
